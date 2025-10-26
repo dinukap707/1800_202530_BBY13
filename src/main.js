16ef61c8c4f0cb7520ee2cec2f827ecf19769519
Jor123kna <jorjaknaus@icloud.com>
2025-10-26 10:49:19 -0700

    okay

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ function sayHello() {
 // document.addEventListener('DOMContentLoaded', sayHello);
 
 function toProfilePage() {
-    window.location.href = "profile.html";
+    window.location.href = "./1800_202530_BBY13/profile.html";
 }
 
 function toSettingPage(){
-    window.location.href = "settings.html";
+    window.location.href = "./1800_202530_BBY13/settings.html";
 }
